616623f834f40a54e964a4c45959cc25f7617934
Zoro <245185056+AlwaysZoro@users.noreply.github.com>
2026-01-10 00:53:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,11 +35,11 @@ logger = logging.getLogger(__name__)
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            name="AshutoshGoswami24",
+            name="ZoroBhaiya",
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
-            workers=50,  # Reduced from 200 for stability
+            workers=50,
             plugins={"root": "plugins"},
             sleep_threshold=15,
         )
